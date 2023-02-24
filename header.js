b4d0ef8f326c2f549c8ef2a68865121d8a485eb3
Juan Ceferino Quintar <quintarjack@gmail.com>
2023-02-24 04:21:28 -0300

    ultimos retoques

--- header.js
+++ header.js
@@ -1,25 +1,13 @@
- 
   
-
-    let marco = document.getElementById('btn-activar')
-      marco.addEventListener('click', () => {
-        
-         var activar = document.getElementById('mostrarMensaje')
-         activar.classList.remove('marco-not')
-         activar.classList.add('marco')
-
-      })
+  
+  function mostrarVentana() {
+    document.getElementById('mostrarMensaje').style.display = 'block';
+    formulario.reset();
     
-   let ocultar = document.getElementById('ocultar') 
-      ocultar.addEventListener('click', () => {
-
-        var ocultar = document.getElementById('mostrarMensaje') 
-         ocultar.classList.remove('marco')
-         ocultar.classList.add('marco-not')
-      })
-   
-   
-  function limpiar(){
-   infoMensaje.reset();
-   console.log("hola");
+     
   }
+
+  function ocultar() {
+    document.getElementById('mostrarMensaje').style.display = 'none';
+  }
+ 
